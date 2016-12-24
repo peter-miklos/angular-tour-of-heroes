9d47f92ba076565a12be7e93f7224dee3f6161e3
Peter Miklos <pt.miklos@gmail.com>
2016-12-24 22:12:41 +0000

    app.module and hero-detail.component updated to show properly the hero details

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,15 +1,15 @@
 import { Component, Input } from '@angular/core';
-import { Hero } from './hero';
+import { Hero }             from './hero';
 
 @Component({
-  selector: 'hero-detail',
+  selector: 'my-hero-detail',
   template: `
-    <div *ngIf="selectedHero">
+    <div *ngIf="hero">
       <h2>{{hero.name}} details!</h2>
       <div><label>id: </label>{{hero.id}}</div>
       <div>
           <label>name: </label>
-          <input [(ngModel)]="selectedHero.name" placeholder="name"/>
+          <input [(ngModel)]="hero.name" placeholder="name"/>
       </div>
     </div>`
 })
